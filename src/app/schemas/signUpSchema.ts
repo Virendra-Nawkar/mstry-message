70ba2b6189937fe51c1928a4bea980a935b818f2
Virendra-Nawkar <virendranawkar1@gmail.com>
2025-05-15 00:22:11 +0530

    Setup of Zod

--- src/app/schemas/signUpSchema.ts
+++ src/app/schemas/signUpSchema.ts
@@ -1,1 +1,13 @@
-history
+import { z } from 'zod';
+
+export const usernameValidation = z
+    .string()
+    .min(2, "Username must be at least 2 characters")
+    .max(20, "Username must be no more than 20 characters")
+    .regex(/^[a-zA-Z0-9_]+$/, "Username must not contain special characters")
+
+export const signUpSchema = z.object({
+    username :  usernameValidation,
+    email : z.string().email({message : 'Invalid Email Address'}),
+    password : z.string().min(6, {message:"Password must be atleast 6 Characters "})
+})
--- src/app/schemas/verifySchema.ts
+++ src/app/schemas/verifySchema.ts
@@ -1,1 +1,5 @@
-sf
+import {z} from 'zod';
+
+export const verifySchema = z.object({
+    code : z.string().length(6, 'Verfication code must be 6 digits')
+})
