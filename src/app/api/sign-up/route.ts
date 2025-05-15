7ba8cc6256b5760a94d80fb300b5514911818519
Virendra-Nawkar <virendranawkar1@gmail.com>
2025-05-15 15:15:40 +0530

    New user Created -v1.49

--- src/app/api/sign-up/route.ts
+++ src/app/api/sign-up/route.ts
@@ -1,1 +1,57 @@
- 
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/User";
+import bcrypt from "bcryptjs";
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+
+export async function POST(request: Request) {
+    await dbConnect()
+
+    try {
+        const { username, email, password } = await request.json()
+        const existingUserVerifiedByUsername = await UserModel.findOne({
+            username,
+            isVerified: true
+        })
+
+        if (existingUserVerifiedByUsername) {
+            return Response.json({
+                success: false,
+                message: "Username is already taken"
+            }, { status: 400 })
+        }
+
+        const existingUserByEmail = await UserModel.findOne({ email });
+        const verifyCode = Math.floor(100000 + Math.random() * 900000).toString(); 
+
+        if (existingUserByEmail) {
+            true  //TODO: back here
+        } else {
+            const hasedPassword = await bcrypt.hash(password, 10)
+            const expiryDate = new Date()
+            expiryDate.setHours(expiryDate.getHours() + 1)
+
+            const newUser = new UserModel({
+                username,
+                email,
+                password : hasedPassword,
+                verifyCode,
+                verifyCodeExpiry : expiryDate,
+                isVerified : false,
+                isAcceptingMessage: true,
+                messages: []
+            })
+
+            await newUser.save();
+        }
+
+    } catch (error) {
+        console.error("Error Refisterting User", error)
+        return Response.json({
+            succuss: false,
+            message: "Error Registering User"
+        },
+            {
+                status: 500
+            })
+    }
+}
