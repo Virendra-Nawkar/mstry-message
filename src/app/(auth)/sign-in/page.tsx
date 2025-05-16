5cc4014fd96e918576b03cd27a62ec7b62c0b244
Virendra-Nawkar <virendranawkar1@gmail.com>
2025-05-16 21:21:49 +0530

    -v5.10

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,40 @@
 "use client";
-import { useSession, signIn, signOut } from "next-auth/react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import Link from "next/link";
+import { useState } from "react";
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import { useDebounceValue } from 'usehooks-ts'
+import { toast } from "sonner"
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    ) 
-  }
+
+const page = () => {
+  const [username, setUsername] = useState('');
+  const [usernameMessage, setUsernameMessage] = useState('')
+  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const debouncedUsername = useDebounceValue(username, 300);
+  const router = useRouter();
   return (
-    <>
-      Not signed in <br />
-      <button onClick={() => signIn()} className="bg-orange-600 px-3 py-1 m-4 rounded-sm">Sign in</button>
-    </>
+    <div>
+      <Button
+        onClick={() =>
+          toast("Event has been created", {
+            description: "Sunday, December 03, 2023 at 9:00 AM",
+            action: {
+              label: "Undo",
+              onClick: () => console.log("Undo"),
+            },
+          })
+        }
+      >
+        Show Toast
+      </Button>
+
+    </div>
   )
 }
+
+export default page
