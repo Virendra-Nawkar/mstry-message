56c14fd8811d5e14939978ab53b0ad22a5cd576f
Virendra-Nawkar <virendranawkar1@gmail.com>
2025-05-16 23:47:17 +0530

    Signup page error fixed -v5.58

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,38 +1,9 @@
-"use client";
-import { zodResolver } from "@hookform/resolvers/zod"
-import Link from "next/link";
-import { useState } from "react";
-import { useForm } from "react-hook-form"
-import * as z from "zod"
-import { useDebounceValue } from 'usehooks-ts'
-import { toast } from "sonner"
-import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
-
+import React from 'react'
 
 const page = () => {
-  const [username, setUsername] = useState('');
-  const [usernameMessage, setUsernameMessage] = useState('')
-  const [isCheckingUsername, setIsCheckingUsername] = useState(false);
-  const [isSubmitting, setIsSubmitting] = useState(false);
-  const debouncedUsername = useDebounceValue(username, 300);
-  const router = useRouter();
   return (
     <div>
-      <Button
-        onClick={() =>
-          toast("Event has been created", {
-            description: "Sunday, December 03, 2023 at 9:00 AM",
-            action: {
-              label: "Undo",
-              onClick: () => console.log("Undo"),
-            },
-          })
-        }
-      >
-        Show Toast
-      </Button>
-
+      page of sign-in
     </div>
   )
 }
